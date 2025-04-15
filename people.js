8e6bfe2be9e4d8d863c4f72c3e7ad3dd05ff505c
Ivanaricci <ivana.ricci26@gmail.com>
2025-04-15 16:21:11 +0200

    [UPD] Add new function

--- people.js
+++ people.js
@@ -2,3 +2,11 @@ const fullName = require('./names')
 const allHobbies = require('./hobbies')
 
 
+const info = () => {
+    return {
+        fullName: fullName('Ivana', 'Ricci'),
+        hobbies: allHobbies('Palestra', 'Viaggiare', 'Dormire')
+    }
+}
+
+console.log(info())
